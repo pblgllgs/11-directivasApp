ace8802982d86a9b4371729323297fde994f79a2
pbl.gllgs <pbl.gllgs@gmail.com>
2021-11-04 11:04:41 -0300

    updates directives
    04/11/2021

--- src/app/productos/pages/agregar/agregar.component.ts
+++ src/app/productos/pages/agregar/agregar.component.ts
@@ -9,8 +9,11 @@ import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms'
 })
 export class AgregarComponent implements OnInit {
 
+  public color :string = 'green';
+  public texto1 : string = 'pablo gallegos';
+
   miFormulario: FormGroup = this.fb.group({
-    nombre : [,Validators.required]
+    nombre : ['',[Validators.required,Validators.minLength(4)]]
   });
 
   constructor(private fb:FormBuilder) { }
@@ -22,8 +25,13 @@ export class AgregarComponent implements OnInit {
     return this.miFormulario.get(campo)?.invalid || false;
   }
 
-  guardar(){
+  cambiarNombre(){
+    this.texto1 = Math.random().toString();
+  }
 
+  cambiarColor(){
+    const color = "#xxxxxx".replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    this.color = color;
   }
 
 
--- src/app/shared/directives/error-msg.directive.ts
+++ src/app/shared/directives/error-msg.directive.ts
@@ -1,30 +1,57 @@
-import { Directive, ElementRef, Input, OnInit } from '@angular/core';
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Directive({
   selector: '[error-msg]'
 })
 export class ErrorMsgDirective implements OnInit {
 
+  private _color :string = 'red';
+  private _mensaje:string =  'Este campo es requerido';
+  private _valido:boolean = false;
+
   htmlElement: ElementRef<HTMLElement>;
-  @Input() color: string = 'red';
-  @Input() mensaje: string = 'Este campo es necesario';
+
+  @Input() set color(valor:string){
+    this._color = valor;
+    this.setColor();
+  }
+
+  @Input() set mensaje(valor:string){
+    this._mensaje = valor;
+    this.setMensaje();
+  }
+
+  @Input() set valido(noTieneError:boolean){
+    if(noTieneError === true){
+      this.htmlElement.nativeElement.classList.add('hidden');
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hidden');
+      this.htmlElement.nativeElement.classList.add('animate__animated');
+      this.htmlElement.nativeElement.classList.add('animate__fadeIn');
+    }
+  }
 
   constructor(private el:ElementRef<HTMLElement>) {
     this.htmlElement = el;
    }
 
   ngOnInit(): void {
+    this.setEstilo();
     this.setColor();
     this.setMensaje();
   }
 
-  setColor():void{
-    this.htmlElement.nativeElement.style.color = this.color;
+  setEstilo():void{
     this.htmlElement.nativeElement.classList.add('form-text');
   }
 
+  setColor():void{
+    this.htmlElement.nativeElement.style.color = this._color;
+    
+  }
+
   setMensaje():void{
-    this.htmlElement.nativeElement.innerText = this.mensaje;
+    this.htmlElement.nativeElement.innerText = this._mensaje;
   }
 
 }
